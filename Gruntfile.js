de9e253ea02f5800fb4c8e61a309cb1061deb3ba
Paden <padenportillo@gmail.com>
2014-02-23 12:11:03 -0600

    Remove travis-ci for gruntbuildcontrol

--- Gruntfile.js
+++ Gruntfile.js
@@ -332,6 +332,24 @@ module.exports = function (grunt) {
                 'imagemin',
                 'svgmin'
             ]
+        },
+        buildcontrol:
+        {
+            options:
+            {
+                dir     : 'dist',
+                commit  : true,
+                push    : true,
+                message : 'Built %sourceName% from commit %sourceCommit% on branch %sourceBranch%'
+            },
+            pages:
+            {
+                options:
+                {
+                    remote: 'git@github.com:Paden/Paden.github.io.git',
+                    branch: 'master'
+                }
+            }
         }
     });
 
